aa2b3de1f71d49f7ce1ad3382f5ee47cceec996c
LEANDRO GARCIA <leandrogarcia.net@hotmail.com>
2023-11-19 00:46:44 -0500

    first commit

---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [CommonModule,RouterModule ],
+  templateUrl: './sidemenu.component.html',
+  styles: ``
+})
+export class SidemenuComponent {
+
+public menuItems = routes
+.map(route => route.children ?? [])
+.flat()
+.filter(route => route && route.path)
+.filter(route => !route.path?.includes(':'));
+
+constructor() {
+
+  // const dashboardRoutes = routes
+  // .map(route => route.children ?? [])
+  // .flat()
+  // .filter(route => route && route.path)
+  // .filter(route => !route.path?.includes(':'));
+
+  // console.log(dashboardRoutes);
+}
+
+
+}
